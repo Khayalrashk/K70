f4d3e8b8f1669d861084202c83e66dff74e5af2d
ALPOP <159054026+DEVALPOP@users.noreply.github.com>
2024-02-07 00:43:11 +0200

    Update play.py

--- MatrixMusic/plugins/play/play.py
+++ MatrixMusic/plugins/play/play.py
@@ -33,7 +33,7 @@ from config import BANNED_USERS, lyrical
             "تشغيل",
             "فيديو",
             "/play",
-            "/vplay",
+            "فيديو",
             "/cplay",
             "/cvplay",
             "/playforce",
