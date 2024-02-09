c0d4dc001c01c11b8c2911538b50b79c31c887b1
oreopr0 <123789155+oreopr0@users.noreply.github.com>
2024-02-10 01:46:01 +0300

    Update marobot.py

--- MatrixMusic/plugins/play/marobot.py
+++ MatrixMusic/plugins/play/marobot.py
@@ -7,7 +7,6 @@ from MatrixMusic import app
 # Replace the following line with your actual OWNER_ID
 OWNER_ID = 956893993
 
-
 @app.on_message(filters.command(['بوت'], prefixes=""))
 async def Italymusic(client: Client, message: Message):
     me = await client.get_me()
@@ -34,10 +33,14 @@ async def Italymusic(client: Client, message: Message):
         print(e)
         rank = "مش عرفنلو مله ده😒"
     
-    # Fetch user's profile photos
-    photos = await client.get_profile_photos(user_id, limit=1)
-    if photos.total_count > 0:
-        photo = photos.photos[0]
-        await client.send_photo(chat_id, photo.file_id, caption=f"""**نعم حبيبي :** {italy} 🥰❤\n**انا اسمي القميل :** {bot_name} 🥺🙈\n**رتبتك هي :** {rank}""", reply_markup=keyboard)
-    else:
-        await message.reply_text("لا يوجد صورة ملف تعريفية متاحة.")
+    await message.reply_text(f"أرسل لي صورة ملف تعريفية حتى أستطيع عرضها مع رسالتك.")
+
+    # Wait for the user to send their profile photo
+    photo_message = await client.listen(filters.photo & filters.private)
+
+    # Get the photo file id from the received photo message
+    photo_file_id = photo_message.photo.file_id
+
+    # Reply to the user with their profile photo
+    await message.reply_photo(photo_file_id, caption=f"""**نعم حبيبي :** {italy} 🥰❤\n**انا اسمي القميل :** {bot_name} 🥺🙈\n**رتبتك هي :** {rank}""", reply_markup=keyboard)
+
